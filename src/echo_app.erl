b0160365da1a0ac85d52aafe23b8356be0f76b09
Leandro Ostera <me@leostera.com>
2015-03-18 03:09:18 -0300

    adds start function to OTP application

--- src/echo_app.erl
+++ src/echo_app.erl
@@ -2,3 +2,15 @@
 -author(ostera).
 
 -behavior(application).
+
+%% application callbacks
+
+-export([
+         start/2,
+         stop/1
+        ]).
+
+start(_Type, _Args) ->
+    PrivDir = code:priv_dir(echo_app),
+    {ok, App} = application:get_env(echo_app, app),
+    echo_sup:start_link(filename:join([PrivDir, App])).

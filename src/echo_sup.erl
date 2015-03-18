f3b92cae5ae8e7a9328ba088199578ab35235b16
Leandro Ostera <me@leostera.com>
2015-03-18 02:52:49 -0300

    adds supervisor

---
+++ src/echo_sup.erl
@@ -0,0 +1,18 @@
+-module(echo_sup).
+-author(ostera).
+
+-behavior(supervisor).
+
+%% External exports
+-export([start_link/1]).
+
+%% supervisor callbacks
+-export([init/1]).
+
+start_link(App) -> 
+    supervisor:start_link(echo_sup, App).
+
+init(App) ->
+    {ok, {{one_for_one, 3, 10},
+          [{echo, {echo, start_link, [App]},
+            permanente, 10, worker, [echo]}]}}.
